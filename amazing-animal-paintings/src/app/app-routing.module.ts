d0349b66fb674771809424cac02703424209c41e
James Luu <jamesluu0917@gmail.com>
2024-08-31 21:00:56 -0400

    Set up routing + api mock

--- amazing-animal-paintings/src/app/app-routing.module.ts
+++ amazing-animal-paintings/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './product/product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // home page redirects instantly to products
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  // shows product-list component on the /products page
+  { path: 'products', component: ProductListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- amazing-animal-paintings/src/app/product/product.service.ts
+++ amazing-animal-paintings/src/app/product/product.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../models/product';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  // link of /products page
+  private apiUrl = environment.apiUrl + '/products';
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getProducts(): Observable<Product[]> {
+    // get request from apiUrl
+    return this.http.get<Product[]>(this.apiUrl);
+  }
 }
