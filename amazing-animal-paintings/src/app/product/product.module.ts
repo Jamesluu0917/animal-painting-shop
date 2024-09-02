85a511e01d99a9ff99ec58d21ce501b04663d10c
James Luu <jamesluu0917@gmail.com>
2024-09-02 15:23:11 -0400

    Added snackbar for Add to cart

--- amazing-animal-paintings/src/app/product/product.module.ts
+++ amazing-animal-paintings/src/app/product/product.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
 import { MatCardModule } from '@angular/material/card';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FlexModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [ProductListComponent],
-  imports: [CommonModule, MatCardModule, FlexModule],
+  imports: [CommonModule, MatCardModule, MatSnackBarModule, FlexModule],
 })
 export class ProductModule {}
